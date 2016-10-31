d5cc42555ced69494cbeffc1530b5febd44df9dd
Ridzick81613 <MRytz55@gmail.com>
2016-10-31 11:20:15 -0500

    Made logo, finalized index page, and downloaded pg-promise to my project folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
